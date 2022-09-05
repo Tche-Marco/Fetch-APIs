732da1dae3c5c385b3efe5cbc8a71f192ff0cc01
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-09-05 15:55:59 -0300

    Adição do mapa

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,3 @@
-async function iniciar() {
-  await apresentarLista()
-}
-
 async function carregarDados() {
   try {
     const response = await fetch('/json/estados.json')
@@ -15,11 +11,26 @@ async function carregarDados() {
 async function apresentarLista() {
   const estados = await carregarDados()
   ul = document.querySelector('#lista')
+
   estados.forEach(estado => {
     li = document.createElement('li')
     li.innerHTML = `${estado.uf} - ${estado.nome}`
     ul.appendChild(li)
   })
 }
+async function criarMapa() {
+  const mapa = new google.maps.Map(document.querySelector('#map'), {
+    zoom: 4,
+    center: { lat: -14.2400732, lng: -53.1805017 } //Brasil
+  })
 
-document.addEventListener('DOMContentLoaded', iniciar)
+  const estados = await carregarDados()
+  estados.forEach(estado => {
+    const marcador = new google.maps.Marker({
+      position: { lat: estado.latitude, lng: estado.longitude },
+      map: mapa
+    })
+  })
+}
+
+document.addEventListener('DOMContentLoaded', apresentarLista())
